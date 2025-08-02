b7cfd5a28e0815e3f01ac0793d14f3a85118ed59
wh0ohm <wararat.boonchieng@gmail.com>
2025-08-02 16:06:54 +0700

    done

---
+++ src/components/TaskInput.tsx
@@ -0,0 +1,19 @@
+export function TaskInput() {
+    return (
+      <div className="m-2 p-2">
+        <h2>Wararat Boonchieng</h2>
+        <form className="row">
+          <div className="col-10">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Insert a task here.."
+            ></input>
+          </div>
+          <div className="col-2 ">
+            <button className="btn btn-primary">Add</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
